6b9731fa0cccda1244ffa96685ee86c4c7655b93
Mohamed Farhat <mfarhat@umbrella.ly>
2022-04-19 17:56:58 +0200

    add api key

--- src/pages/home/map/map.js
+++ src/pages/home/map/map.js
@@ -55,7 +55,7 @@ const map = (props) => {
         <MyMapComponent
             {...props}
             isMarkerShown
-            googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+            googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBk3aFNasenVAyx12Q6qSPKJrthxHm7D4Q&v=3.exp&libraries=geometry,drawing,places"
             loadingElement={<div style={{ height: `100%` }} />}
             containerElement={<div style={{ height: `400px` }} />}
             mapElement={<div style={{ height: `100%` }} />}
